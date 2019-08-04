0f5ed61ae9881e5aba140572e0d0926784db8d34
Masaki Kawabata Neto <masaki.kawabata@gmail.com>
2019-08-04 14:24:34 -0300

    implementacao inicial

--- src/Cache.php
+++ src/Cache.php
@@ -1,55 +1,75 @@
 <?php
+namespace Uspdev\cache;
 
-class cache
+class Cache
 {
+    // default values for expiry
+    public $longExpiry = 4 * 60 * 60; // expiry in 4 hours
+    public $shortExpiry = 10 * 60; //10 minutes
 
-    public function __construct(object $ClassToBeCached)
+    public function __construct(object $classToBeCached = null)
     {
-        $this->cache = new \Memcached();
-        if (empty($this->cache->getServerList())) {
-            $this->cache->addServer('127.0.0.1', 11211);
+        // vamos conectar o servidor memcached local
+        $cache = new \Memcached();
+        if (empty($cache->getServerList())) {
+            $cache->addServer('127.0.0.1', 11211);
         }
-        $this->longExpiry = 4 * 60 * 60; // expira em 4 horas
+        if (!$cache->getVersion()) {
+            die('sem memcached');
+        }
+        $this->cache = $cache;
 
         // vamos injetar a chasse que queremos cachear
-        $this->cachedClass = $ClassToBeCached;
+        $this->cachedClass = $classToBeCached;
     }
 
     public function getCached(string $cachedMethod, $param = null)
     {
-        // vamos criar uma chave adequada dependente dos parametros
-        $paramString = is_array($param) ? implode('-', $param) : $param;
-        $cacheKey = get_class($this->cachedClass) . '-' . $cachedMethod . '-' . $paramString;
+        // criar chave
+        $this->setCacheKey($cachedMethod, $param);
 
-        $data = $this->cache->get($cacheKey);
+        // verifica o cache
+        $data = $this->cache->get($this->cacheKey);
         $this->cacheStatus = $this->cache->getResultCode();
 
-        if ($this->cacheStatus != Memcached::RES_SUCCESS) {
-            // não está no cache, vamos buscar na classe e colocar no cache
+        if ($this->cacheStatus != \Memcached::RES_SUCCESS) {
+            // não está no cache ou está expirado, vamos buscar na classe e colocar no cache
             $data = $this->getRaw($cachedMethod, $param);
         }
-        
+
         return $data;
     }
 
     public function getRaw(string $cachedMethod, $param = null)
     {
         // vamos pegar os dados sem verificar o cache
-        if ($param) {
-            $data = $this->cachedClass->$cachedMethod($param);
+        if (strpos($cachedMethod, '::')) {
+            // estático
+            $data = $param ? $cachedMethod($param) : $cachedMethod();
         } else {
-            $data = $this->cachedClass->$cachedMethod();
+            // instanciado
+            $data = $param ? $this->cachedClass->$cachedMethod($param) : $this->cachedClass->$cachedMethod();
         }
 
-        // vamos criar uma chave adequada dependente dos parametros
-        $paramString = is_array($param) ? implode('-', $param) : $param;
-        $cacheKey = get_class($this->cachedClass) . '-' . $cachedMethod . '-' . $paramString;
+        // criar chave
+        $this->setCacheKey($cachedMethod, $param);
 
         // e vamos colocar no cache
-        $this->cache->set($cacheKey, $data, $this->longExpiry);
+        $this->cache->set($this->cacheKey, $data, $this->longExpiry);
         $this->cacheStatus = $this->cache->getResultCode();
 
         return $data;
+    }
 
+    private function setCacheKey(string $cachedMethod, $param = null)
+    {
+        // vamos criar uma chave adequada dependente dos parametros
+        $paramString = is_array($param) ? implode('-', $param) : $param;
+
+        if (empty($this->cachedClass)) {
+            $this->cacheKey = $cachedMethod . '-' . $paramString;
+        } else {
+            $this->cacheKey = get_class($this->cachedClass) . '-' . $cachedMethod . '-' . $paramString;
+        }
     }
 }
